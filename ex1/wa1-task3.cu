614d3cf10d67ab0b39d038a74b321d0c43dde407
Axel Schwarzenbach <axels@bluewin.ch>
2022-09-13 12:56:20 +0200

    a

--- ex1/wa1-task3.cu
+++ ex1/wa1-task3.cu
@@ -16,9 +16,10 @@ __global__ void funcKernel(float* d_in, float *d_out, int N) {
 
 int timeval_subtract(struct timeval* result, struct timeval* t2,struct timeval* t1) { 
     unsigned int resolution=1000000;
-    long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec) ;
+    long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
     result->tv_sec = diff / resolution;   result->tv_usec = diff % resolution;
     return (diff<0);
+}
 
 #define GPU_RUNS 100
 int main(int argc, char** argv) {
