c482431ef5d3d6639149db51e315fb3980fc5b5f
Axel Schwarzenbach <axels@bluewin.ch>
2022-09-27 11:41:15 +0200

    a

--- w2-code-handin/cuda/pbbKernels.cu.h
+++ w2-code-handin/cuda/pbbKernels.cu.h
@@ -240,8 +240,8 @@ scanIncBlock(volatile typename OP::RedElTp* ptr, const unsigned int idx) {
     */
 
      // 3. scan again the first warp
-    if (warpid == 0) scanIncWarp<OP>(ptr, (idx%32));
-    __syncthreads();
+    if (warpid == 0) scanIncWarp<OP>(ptr, idx);
+    //__syncthreads();
 
     // 4. accumulate results from previous step;
     if (warpid > 0) {
